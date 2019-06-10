a4272a51e6c5bbdf3c1b9a8ecfd5c94dae410d55
vikram <vsskkv@gmail.com>
2019-06-10 22:41:49 +0100

    Update function contains the up and down arrow keys along side the enter key, press code

--- number wizard/Assets/NumberWizard.cs
+++ number wizard/Assets/NumberWizard.cs
@@ -13,11 +13,21 @@ public class NumberWizard : MonoBehaviour
         Debug.Log("Pick a number...");
         Debug.Log("Highest number is: " + max);
         Debug.Log("Lowest is: " + min);
+        Debug.Log("Tell me if its higher or lower....");
+        Debug.Log("Push up = higher, push down = lower");
     }
 
+
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            Debug.Log("Up Arrow key was pressed.");
+        }
+        else if(Input.GetKeyDown(KeyCode.DownArrow)) {
+            Debug.Log("Down Arrow key was pressed.");
+        }
+        else if (Input.GetKeyDown(KeyCode.Return)){
+            Debug.Log("Return key was pressed.");
+        }
     }
 }
