ae9465a383aacc0b401e845f2cfb676b970f0ca0
vikram <vsskkv@gmail.com>
2019-06-10 22:23:25 +0100

    Number Wizard game created and started. Set up the intro debug.log('s)

---
+++ number wizard/Assets/NumberWizard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welcome to Number wizard, yo");
+        Debug.Log("Pick a number...");
+        Debug.Log("Highest number is 1000");
+        Debug.Log("Lowest is 1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
