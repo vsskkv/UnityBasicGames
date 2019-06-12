016ca78daaa602934dcc1e5dada0a917891f1419
vikram <vsskkv@gmail.com>
2019-06-12 19:43:07 +0100

    Connected the state to the UI

--- Text101/Assets/AdventureGame.cs
+++ Text101/Assets/AdventureGame.cs
@@ -7,12 +7,15 @@ public class AdventureGame : MonoBehaviour
 {
     //var
     //SerializeField means that its in the inspector
-    [SerializeField] Text textComponent; 
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
 
+    State state;
 
     // Start is called before the first frame update
     void Start(){
-        textComponent.text = ("Start Game");
+        state = startingState;
+        textComponent.text = state.GetStateStory();
     }
 
     // Update is called once per frame
--- Text101/Assets/State.cs
+++ Text101/Assets/State.cs
@@ -7,4 +7,8 @@ using UnityEngine;
 public class State : ScriptableObject
 {
     [ TextArea(14,10)] [SerializeField] string storyText;
+
+    public string GetStateStory() {
+        return storyText;
+    }
 }
