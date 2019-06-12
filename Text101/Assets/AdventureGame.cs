63cbe38c461090585d21cbb0feddf6fc68dc64c0
vikram <vsskkv@gmail.com>
2019-06-12 18:27:44 +0100

    added the text on the screen through a script

---
+++ Text101/Assets/AdventureGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class AdventureGame : MonoBehaviour
+{
+    //var
+    //SerializeField means that its in the inspector
+    [SerializeField] Text textComponent; 
+
+
+    // Start is called before the first frame update
+    void Start(){
+        textComponent.text = ("Start Game");
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+}
